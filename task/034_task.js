70e097e765b0ebe9bf93dc023a554941191a0c2f
akshay037 <akshayteltumbade69@gmail.com>
2023-11-09 08:10:15 +0530

    Complete task 034

---
+++ task/034_task.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Sort Words in Alphabetical Order
+
+const string = prompt('Enter a sentence: ');
+
+const words = string.split(' ');
+
+words.sort();
+
+console.log('The sorted words are:');
+
+for (const element of words) {
+    console.log(element);
+}
