1219b31b49f8d664d2151b5064b8e427069d3ade
akshay037 <akshayteltumbade69@gmail.com>
2023-12-03 07:42:08 +0530

    Complete task 056

---
+++ task/056_task.js
@@ -0,0 +1,10 @@
+// JavaScript Program to Check Whether a String Contains a Substring
+
+const str = prompt('Enter a string:');
+const checkString = prompt('Enter a string that you want to check:');
+
+if (str.includes(checkString)) {
+    console.log(`The string contains ${checkString}`);
+} else {
+    console.log(`The string does not contain ${checkString}`);
+}
