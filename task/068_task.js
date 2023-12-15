d798c3cccfa665422d86a9ecab92d24a11ad04d9
akshay037 <akshayteltumbade69@gmail.com>
2023-12-15 21:30:29 +0530

    Complete task 068

---
+++ task/068_task.js
@@ -0,0 +1,12 @@
+// JavaScript Program to Check if An Array Contains a Specified Value
+
+const array = ['you', 'will', 'learn', 'javascript'];
+
+const hasValue = array.includes('javascript');
+
+// check the condition
+if (hasValue) {
+    console.log('Array contains a value.');
+} else {
+    console.log('Array does not contain a value.');
+}
