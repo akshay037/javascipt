0a00a5350ad46f8fb245e988e334ce7df89e7a3d
akshay037 <akshayteltumbade69@gmail.com>
2024-01-03 09:34:23 +0530

    Complete task 083

---
+++ task/083_task.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Get File Extension
+
+function getFileExtension(filename) {
+
+    // get file extension
+    const extension = filename.split('.').pop();
+    return extension;
+}
+
+// passing the filename
+const result1 = getFileExtension('module.js');
+console.log(result1);
+
+const result2 = getFileExtension('module.txt');
+console.log(result2);
