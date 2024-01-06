51c203bf9ac6199d1edaaf6788f4698f362b73d6
akshay037 <akshayteltumbade69@gmail.com>
2024-01-06 07:41:28 +0530

    Complete task 086

---
+++ task/086_task.js
@@ -0,0 +1,17 @@
+// JavaScript Program to Illustrate Different Set Operations
+
+function union(a, b) {
+    let unionSet = new Set(a);
+    for (let i of b) {
+        unionSet.add(i);
+    }
+    return unionSet
+}
+
+// two sets of fruits
+const setA = new Set(['apple', 'mango', 'orange']);
+const setB = new Set(['grapes', 'apple', 'banana']);
+
+const result = union(setA, setB);
+
+console.log(result);
