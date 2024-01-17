84fdc27bcecc93901090dd9c1ce2cf5f74de7485
akshay037 <akshayteltumbade69@gmail.com>
2024-01-17 21:59:31 +0530

    Complete task 099

---
+++ task/099_task.js
@@ -0,0 +1,18 @@
+// JavaScript Program to Pass a Function as Parameter
+
+function greet() {
+    return 'Hello';
+}
+
+// passing function greet() as a parameter
+function name(user, func) {
+
+    // accessing passed function
+    const message = func();
+
+    console.log(`${message} ${user}`);
+}
+
+name('John', greet);
+name('Jack', greet);
+name('Sara', greet);
