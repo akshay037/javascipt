8a141c0d995116eefe5c58ee55a873fb0a1e7824
akshay037 <akshayteltumbade69@gmail.com>
2024-01-25 06:36:22 +0530

    Practice of day 003

---
+++ practice/003_day.js
@@ -0,0 +1,12 @@
+const arr = [1, 5, 4, 12, 15, 9]
+
+const sumOfOdd = () => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            sum += arr[i]
+        }
+    }
+    return sum
+}
+console.log(sumOfOdd())
