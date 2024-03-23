dc9c5eaebfb8e1dbefe14f15656feacb4913dfec
akshay037 <akshayteltumbade69@gmail.com>
2024-03-23 07:19:25 +0530

    Practice of day 056

---
+++ practice/056_day.js
@@ -0,0 +1,16 @@
+let myTime = 9;
+let output;
+if (myTime >= 8 && myTime < 12) {
+    output = "Wake up, it's morning";
+} else if (myTime >= 12 && myTime < 13) {
+    output = "Go to lunch";
+} else if (myTime >= 13 && myTime <= 16) {
+    output = "Go to work";
+} else if (myTime > 16 && myTime < 20) {
+    output = "Dinner time";
+} else if (myTime >= 22) {
+    output = "Time to go to sleep";
+} else {
+    output = "You should be sleeping";
+}
+console.log(output)
