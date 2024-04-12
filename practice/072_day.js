12db307bab66e5c5c2b38a4efd1f4607f66b3cf8
akshay037 <akshayteltumbade69@gmail.com>
2024-04-12 07:49:37 +0530

    Practice of day 072

---
+++ practice/072_day.js
@@ -0,0 +1,5 @@
+let uri = "https://www.example.com/submit?name=maaike van putten";
+let encoded_uri = encodeURI(uri);
+console.log("Encoded:", encoded_uri);
+let decoded_uri = decodeURI(encoded_uri);
+console.log("Decoded:", decoded_uri)
