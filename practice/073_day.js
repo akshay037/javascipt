ef8a20c6c8f20d5ae52edd3902b35ae9bb7a6b05
akshay037 <akshayteltumbade69@gmail.com>
2024-04-13 09:49:29 +0530

    Practice of day 073

---
+++ practice/073_day.js
@@ -0,0 +1,6 @@
+let str_float = "7.6";
+let int_float = parseInt(str_float);
+console.log("Type of", int_float, "is", typeof int_float);
+let str_binary = "0b101";
+let int_binary = parseInt(str_binary);
+console.log("Type of", int_binary, "is", typeof int_binary)
