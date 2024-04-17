bc902cb39e9618452bd6199cfa8f60aeb3207c06
akshay037 <akshayteltumbade69@gmail.com>
2024-04-17 07:23:07 +0530

    Practice of day 077

---
+++ practice/077_day.js
@@ -0,0 +1,17 @@
+let x = 6.78;
+let y = 5.34;
+console.log("X:", x, "becomes", Math.round(x));
+console.log("Y:", y, "becomes", Math.round(y));
+console.log("X:", x, "becomes", Math.ceil(x));
+console.log("Y:", y, "becomes", Math.ceil(y));
+console.log("X:", x, "becomes", Math.trunc(x));
+console.log("Y:", y, "becomes", Math.trunc(y))
+console.log("X:", x, "becomes", Math.floor(x));
+console.log("Y:", y, "becomes", Math.floor(y))
+
+let negativeX = -x;
+let negativeY = -y;
+console.log("negativeX:", negativeX, "becomes", Math.ceil(negativeX));
+console.log("negativeY:", negativeY, "becomes", Math.ceil(negativeY));
+console.log("negativeX:", negativeX, "becomes", Math.floor(negativeX));
+console.log("negativeY:", negativeY, "becomes", Math.floor(negativeY));
